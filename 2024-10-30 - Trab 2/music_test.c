16d9d2b01927c649f8737697033b5ab7d06caa36
Nuno Oliveira <nuno.oliveira@isel.pt>
2024-10-30 00:21:18 +0000

    actualiza aula 30-10-2024

--- 2024-10-30 - Trab 2/music_test.c
+++ 2024-10-30 - Trab 2/music_test.c
@@ -1,10 +1,28 @@
 #include "music_db.h"
 
-int main() {
-    db_artist_add("Queen", 86, "rock");
+
+int main() 
+{
+    // // teste 1
+    // artist_id_t aid = db_artist_add("Queen", 86, "Classic Rock");
+    // db_artist_print(aid);
+
+    // // teste 2
+    // date_t release_date = date_from_str("5-2-1991");
+    // album_id_t album_id = db_album_add("Innuendo (Deluxe Edition 2011 Remaster)", release_date, aid);
+    // db_album_print(album_id);
+
+    // // teste 3
+    // db_track_add("Innuendo - Remastered 2011", 393, album_id);
+    // db_track_add("I'm Going Slightly Mad - Remastered 2011", 262, album_id);
+    // db_track_add("Headlong - Remastered 2011", 277, album_id);
+
+    // // teste 4
+    // aid= db_artist_add("Ed Sheeran", 89, "pop");
+    // db_artist_print(aid);
+
     
-    date_t d = { .day = 1, .month = 6, .year = 1888};
-    db_album_add("Magic", d, 0);
-    db_album_add("Magic2", d, 0);
-    db_dump();
+    // db_dump();
+
+    return 0;
 }
